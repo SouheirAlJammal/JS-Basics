f2bf0b9b23a8c881b9b503839b95645d300c800f
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-08-31 12:12:26 +0300

    completed JS_1 Step_4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,7 +1,8 @@
 var Name, Surname, City;
 Name = prompt("Please Enter Your Name: ");
 Surname = prompt("what is your Surname? ");
-alert(" Hello, " + Name + " " + Surname);
 City = prompt(" In which city did you live? ");
-alert('Yes "' + City + '" is a nice city to live in, well choosen ' + Name + " " + Surname);
+alert("Nom: "+Surname+"\nPrenom: "+Name+"\nVille: "+City);
+alert("Hello,"+Name);
+
 
