bdba81c77e71c81258c0fed30b90369591cf5761
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-08-31 12:55:31 +0300

    completed JS_1 Step_7

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,3 +1,13 @@
-var shoe_size,birth_year;
-shoe_size=prompt("Please enter your shoe size :");
-birth_year=prompt("Please enter your birth year");
+
+function MultipleOperations(num1,num2) {
+    let result = ((num1 * 2 + 5) * 50) - num2 + 1766;
+
+
+    return result
+}
+
+var shoe_size, birth_year;
+shoe_size = prompt("Please enter your shoe size :");
+birth_year = prompt("Please enter your birth year");
+
+alert(MultipleOperations(shoe_size,birth_year));
