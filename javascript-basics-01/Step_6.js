2a454e14a8b0910d5e2f47884901316605efc7e4
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-08-31 12:44:51 +0300

    completed JS_1 Step_6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,5 +1,6 @@
-var num1, num2, result;
+var num1, num2, result, remainder;
 num1 = prompt("Please Enter the first number: ");
 num2 = prompt("Please Enter the second numbre: ");
-result = num1 % num2;
-alert("The remainder of " + num1 + " / " + num2 + " is : " + result);
+result = num1 / num2;
+remainder=num1%num2;
+alert("The remainder of " + num1 + " / " + num2 + " is : " + remainder);
---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,3 @@
+var shoe_size,birth_year;
+shoe_size=prompt("Please enter your shoe size :");
+birth_year=prompt("Please enter your birth year");
