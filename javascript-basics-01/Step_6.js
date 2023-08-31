8497c7cadbaee0405de7b5ac76ad285dd6a8006c
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-08-31 12:31:37 +0300

    completed JS_1 Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,5 @@
+var num1, num2, result;
+num1 = prompt("Please Enter the first number: ");
+num2 = prompt("Please Enter the second numbre: ");
+result = num1 / num2;
+alert("The result of " + num1 + " / " + num2 + " is : " + result);
