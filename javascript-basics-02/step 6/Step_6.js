3841c610977230d741b999410414d8435e4e934d
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-09-01 10:35:03 +0300

    completed JS_2 Step_6

--- javascript-basics-02/step 6/Step_6.js
+++ javascript-basics-02/step 6/Step_6.js
@@ -1,20 +1,36 @@
 
 let images = document.querySelectorAll("img");
 
-images.forEach((image, i) => {
 
+for(let i=0; i<images.length;i++){
 
-    image.addEventListener('mouseover', function () {
-
-        image.src = "images/image" + (Math.floor(Math.random() * images.length) + 1) + ".jpg";
+    images[i].addEventListener('mouseover', function () {
+    
+        images[i].src = "images/image" + (Math.floor(Math.random() * images.length) + 1) + ".jpg";
     })
+    
+
+    images[i].addEventListener('mouseout', function () {
+
+                images[i].src = "images/image" + (i+1) + ".jpg";
+            })
+    
+    }
+
+// images.forEach((image, i) => {
 
 
-    image.addEventListener('mouseout', function () {
+//     image.addEventListener('mouseover', function () {
 
-        image.src = "images/image" + (i+1) + ".jpg";
-    })
+//         image.src = "images/image" + (Math.floor(Math.random() * images.length) + 1) + ".jpg";
+//     })
 
 
-});
+//     image.addEventListener('mouseout', function () {
+
+//         image.src = "images/image" + (i+1) + ".jpg";
+//     })
+
+
+// });
 
